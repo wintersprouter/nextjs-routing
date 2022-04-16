a96389e6c879f47815d7829435f468ab3ce7ef35
wintersprouter <bornonefreekiss@alum.ccu.edu.tw>
2022-04-16 23:16:15 +0800

    feat: navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -2,9 +2,17 @@ import { useRouter } from 'next/router'
 function ClientProjectsPage() {
   const router = useRouter()
   console.log(router.query)
+  function loadProjectHandler() {
+    //load data...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'apple', clientprojectid: 'project1' }
+    })
+  }
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
