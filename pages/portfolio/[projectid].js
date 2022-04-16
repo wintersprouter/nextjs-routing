e45327ea9341dd675e131c916bef751cc7272073
wintersprouter <bornonefreekiss@alum.ccu.edu.tw>
2022-04-16 22:24:31 +0800

    feat: extracting dynamic path segment data

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router'
 function PortfolioProjectPage() {
+  const router = useRouter()
+  console.log(router.pathname)
+  console.log(router.query)
+  //像後端伺服器發送請求
+  //根據 router.query.projectid 以取得特定 id 的資料
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
